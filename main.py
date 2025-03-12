2dbe880cb538d76e9f2b4e4ed6196736a5c703ca
Fatih Gül <fatihgulrose@outlook.com>
2025-03-12 19:12:41 +0300

    Number recognition

---
+++ main.py
@@ -0,0 +1,42 @@
+import tensorflow as tf
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+y_train = keras.utils.to_categorical(y_train, 10)
+y_test = keras.utils.to_categorical(y_test, 10)
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),  # 28x28 resmi düzleştir
+    keras.layers.Dense(128, activation='relu'),  # 128 nöronlu gizli katman
+    keras.layers.Dense(10, activation='softmax')  # 10 sınıf için çıkış katmanı
+])
+
+model.compile(optimizer='adam',
+              loss='categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=10, validation_data=(x_test, y_test))
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"Test Doğruluğu: {test_acc:.4f}")
+
+num_samples = 5
+sample_images = x_test[:num_samples]
+sample_labels = np.argmax(y_test[:num_samples], axis=1)
+
+predictions = np.argmax(model.predict(sample_images), axis=1)
+
+plt.figure(figsize=(10, 5))
+for i in range(num_samples):
+    plt.subplot(1, num_samples, i + 1)
+    plt.imshow(sample_images[i], cmap='gray')
+    plt.title(f"Gerçek: {sample_labels[i]}\nTahmin: {predictions[i]}")
+    plt.axis('off')
+plt.show()
